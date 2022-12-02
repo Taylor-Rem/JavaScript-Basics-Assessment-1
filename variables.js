805e44ca556e5ab044f8b8fc2141212f3d74826a
Taylor Remund <remundtaylor3@gmail.com>
2022-12-02 10:11:42 -0700

    variables finished

--- variables.js
+++ variables.js
@@ -1,17 +1,23 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
+//CODE HERE
+
+let myName = "Taylor Remund";
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
 
+let faveNum = 24;
+
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+
+let lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -22,9 +28,15 @@
 
 //CODE HERE
 
+console.log(faveNum === 13 ? "lucky 13!" : "not lucky 13");
+
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
 //CODE HERE
+
+for (let i = 0; i < 5; i++) {
+  console.log(faveNum);
+}
